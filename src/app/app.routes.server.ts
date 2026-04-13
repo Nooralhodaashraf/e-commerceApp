1b84b273616f6ed651a0c1e378f47f4990c597c9
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-04-13 16:27:13 +0200

    feat: add server routes for details, checkout, and category navigation

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'details/:id/:slug', renderMode: RenderMode.Client },
+  { path: 'checkout/:id', renderMode: RenderMode.Client },
+  { path: 'spicifcCatigory/:id', renderMode: RenderMode.Client },
+  { path: 'brandproducts/:id', renderMode: RenderMode.Client },
+  { path: 'cart', renderMode: RenderMode.Client },
+  { path: 'allorders', renderMode: RenderMode.Client },
+
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
