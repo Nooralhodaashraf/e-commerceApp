5adb0388dceeec8b052c3a6f5b7b692cefa83e8a
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-04-13 16:55:00 +0200

    fix: change renderMode from Prerender to Server for fallback route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -9,6 +9,6 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'allorders', renderMode: RenderMode.Client },
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
 ];
